5880bbf41a3de8b6c08120d8b2ed258cdaf286ac
Omar Khaled Ahmed <114437079+Omar61554@users.noreply.github.com>
2024-01-04 17:20:23 +0200

    Update slave.ino

--- SRC/slave/slave.ino
+++ SRC/slave/slave.ino
@@ -9,8 +9,13 @@ void setup() {
 }
 
 void loop() {
+  
+  Serial.println("Outside Loop");
+  BLUE_checkBarrier();
   if (Serial.available() > 0) { // Check if there is any data available on the serial port
     char command = Serial.read(); // Read the incoming command
+    Serial.print("Recieved Message : ");
+    Serial.println(command);
     if (command == 'U') { // If the command is 'U', signal from ultrasonic
       //implemnt ultrasonic code
       Serial.println("Ultrasonic message");
@@ -18,11 +23,45 @@ void loop() {
     else if (command=='J'){
     //implement jokerMission  
         Serial.println("Joker mission");
+        
+        SHOT_selectBall('J');
+        SHOT_shoot();
+        digitalWrite(13,LOW);
+        delay(100);
+        digitalWrite(13,HIGH);
+        delay(200);
+        digitalWrite(13,LOW);
+        delay(300);
+        digitalWrite(13,HIGH);
+        
+        delay(1000);
+        digitalWrite(13,LOW);
+        unsigned long tick = millis();
+        unsigned long tock = millis();
+        while((tock - tick) <5000){        
+        Serial.write('D');
+        tock = millis();}
     }
-    else if (command == 'R') { // If the command is '0', turn the lights off
-    //implement riddlerMission
+    else if (command == 'R') { //implement riddlerMission  
         Serial.println("Riddler mission");
-     
-    }
-     
-}}
+        
+        SHOT_selectBall('R');
+        SHOT_shoot();
+        digitalWrite(13,LOW);
+        delay(100);
+        digitalWrite(13,HIGH);
+        delay(200);
+        digitalWrite(13,LOW);
+        delay(300);
+        digitalWrite(13,HIGH);
+        
+        delay(1000);
+        digitalWrite(13,LOW);
+        unsigned long tick = millis();
+        unsigned long tock = millis();
+        while((tock - tick) <5000){        
+        Serial.write('D');
+        tock = millis();}
+        }
+     }
+}
